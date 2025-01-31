1a0110a1183bc0fc528595205edbc209fd2f2c28
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-31 18:06:24 +0100

    feat: add events on click to accordion btn and add edit style

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function Accordion({ id, title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const accordionOpen = () => {
+    setIsOpen((currentValue) => !currentValue);
+  };
+
   return (
     <div key={id} className="accordion">
-      <div className="accordion__top">
+      <div className={`accordion__top ${isOpen && "accordion__top--open"}`}>
         <h3 className="accordion__title">{title}</h3>
-        <button className="accordion__btn">+</button>
+        <button onClick={accordionOpen} className="accordion__btn">
+          {isOpen ? "-" : "+"}
+        </button>
       </div>
-      <div className="accordion__description">{description}</div>
+      {isOpen && <div className="accordion__description">{description}</div>}
     </div>
   );
 }
