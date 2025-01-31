ef59d8b96f4dd0561bd7f1a8fe3f391b7214b184
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-31 17:34:52 +0100

    feat: create accordion component and add style

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,11 @@
+export default function Accordion(id, title, description) {
+  return (
+    <div key={id} className="accordion">
+      <div className="accordion__top">
+        <h3 className="accordion__title">{title}</h3>
+        <button className="accordion__btn">+</button>
+      </div>
+      <div className="accordion__description">{description}</div>
+    </div>
+  );
+}
