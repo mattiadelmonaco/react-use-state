102403d117d91ca71c7f09f1eefe3fdbb4c66768
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-03 19:28:02 +0100

    feat: now when an accordion is open the others are closed (bonus)

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
-
-export default function Accordion({ id, title, description }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const accordionOpen = () => {
-    setIsOpen((currentValue) => !currentValue);
-  };
-
+export default function Accordion({
+  id,
+  title,
+  description,
+  isOpen,
+  onAccordionToggle,
+}) {
   return (
     <div key={id} className="accordion">
-      <div className={`accordion__top ${isOpen && "accordion__top--open"}`}>
+      <div className={`accordion__top ${isOpen ? "accordion__top--open" : ""}`}>
         <h3 className="accordion__title">{title}</h3>
-        <button onClick={accordionOpen} className="accordion__btn">
+        <button onClick={onAccordionToggle} className="accordion__btn">
           {isOpen ? "-" : "+"}
         </button>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import languages from "../data/languages";
 import Accordion from "./Accordion";
 
 export default function Main() {
+  const [activeAccordion, setActiveAccordion] = useState(null);
+
+  const handleActiveAccordion = (id) => {
+    setActiveAccordion((currentId) => {
+      if (currentId === id) {
+        return null;
+      }
+      return id;
+    });
+  };
+
   return (
     <main>
       <section className="container">
@@ -11,6 +23,8 @@ export default function Main() {
               key={language.id}
               title={language.title}
               description={language.description}
+              isOpen={language.id === activeAccordion}
+              onAccordionToggle={() => handleActiveAccordion(language.id)}
             />
           );
         })}
